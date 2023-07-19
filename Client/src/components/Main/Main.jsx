7070bfb482e2fed8e89cb472fe642c13c138a00f
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-07-19 18:21:19 -0500

    animation only on side

--- Client/src/components/Main/Main.jsx
+++ Client/src/components/Main/Main.jsx
@@ -10,12 +10,14 @@ const Main = () => {
 		<Grid
 			container
 			direction="row"
+            xs={12}
 			spacing={0}
 			sx={{ ...styles.mainContainer }}>
-			<Grid item xs direction="column" sx={{ ...styles.bubbles }}>
+
+			<Grid item xs={2} direction="column" sx={{ ...styles.bubbles }}>
 
             <div className="absolute grid grid-cols-12 gap-4 min-h-screen w-full overflow-hidden overflow-hidden-y z-10 " >
-                {[...Array(12)].map((_, i) => (
+                {[...Array(2)].map((_, i) => (
                     <div key={i} className="relative grid min-h-screen place-content-end">
                         {[...Array(4)].map((_, j) => (
                         <div 
@@ -47,7 +49,26 @@ const Main = () => {
 					<div>Contact Me </div>
 				</Grid>
 			</Grid>
-			<Grid item direction="column" xs sx={{ ...styles.bubbles }}></Grid>
+			<Grid item direction="column" xs={2} sx={{ ...styles.bubbles }}>
+            <div className="absolute grid grid-cols-12 gap-4 min-h-screen w-full overflow-hidden overflow-hidden-y z-10 " >
+                {[...Array(10)].map((_, i) => (
+                    <div key={i} className="relative grid min-h-screen place-content-end">
+                        {[...Array(4)].map((_, j) => (
+                        <div 
+                            className={`w-16 h-16 rounded-full bg-white animate-bubble delay-${j+i}s mb-4 absolute`}
+                            key={j}
+                            style={{
+                            '--animation-order': j + 1,
+                            '--animation-length': `${Math.random() * 5 + 3}s`,
+                            '--animation-distance': '100vh',
+                            }}
+                        />
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+            </Grid>
 		</Grid>
 	);
 };
