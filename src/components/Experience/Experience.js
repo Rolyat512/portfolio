8b4ba39431f13f4a19cbe2542bc84dc8dfcd1482
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-08-09 15:54:01 -0500

    added about section and icons

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,38 @@
+import React from 'react'
+// import JavascriptIcon from '@mui/icons-material/Javascript';
+// import CssIcon from '@mui/icons-material/Css';
+// import HtmlIcon from '@mui/icons-material/Html';
+
+// npm install react-icons --save
+import { FaReact } from 'react-icons/fa';
+import { FaHtml5 } from 'react-icons/fa';
+import { FaCss3Alt } from 'react-icons/fa';
+import { TbBrandMongodb } from 'react-icons/tb';
+import { SiGraphql } from 'react-icons/si';
+import { FaNodeJs } from 'react-icons/fa';
+import { SiMui } from 'react-icons/si';
+import { SiMysql } from 'react-icons/si';
+import { FaPhp } from 'react-icons/fa';
+import { RiJavascriptFill } from 'react-icons/ri';
+
+
+
+const Experience = () => {
+  return (
+    <div>
+      <FaHtml5></FaHtml5>
+      <FaCss3Alt></FaCss3Alt>
+      <RiJavascriptFill></RiJavascriptFill>
+      <FaReact></FaReact>
+      <TbBrandMongodb></TbBrandMongodb>
+      <SiGraphql></SiGraphql>
+      <FaNodeJs></FaNodeJs>
+      <SiMui></SiMui>
+      <SiMysql></SiMysql>
+      <FaPhp></FaPhp>
+      
+    </div>
+  )
+}
+
+export default Experience
