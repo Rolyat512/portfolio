8ddbbdb8405f7416d72f108ad2fe7b8fe39f278b
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-09-15 17:47:12 -0500

    updated index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
-import Background from './background';
 import About from './About';
-import Experience from './Experience'
+import Header from './Header';
+import Bubbles from './bubbles';
+import Experience from './Experience';
+import Portfolio from './Portfolio';
+import Resume from './Resume';
+import Socials from './Socials';
+import Contact from './Contact';
 
-export {Background, About, Experience}; 
+
+export {Bubbles, Header, About, Experience, Portfolio, Resume, Socials, Contact}; 
