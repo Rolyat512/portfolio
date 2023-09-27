593504431f638a5de0e62962ac00fc1937144646
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-09-26 19:36:38 -0500

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import { useState } from 'react';
-import { Bubbles, Header, About, Experience, Portfolio, Resume, Socials, Contact } from './components';
+import { Bubbles, Header, Title, About, Portfolio, Resume, Socials, Divider, Contact } from './componets';
 
 const App = () => {
-
   const [page, setPage] = useState('about');
- 
+
   return <>
-    <Header setPage={setPage} />  
+    <Header setPage={setPage} />
 
-    <Bubbles >
-
-        {page == 'about' ? <About />: page == 'experience' ? <Experience /> : <Portfolio />}
-      
-        {/* <Contact /> */}
-
-      {/* <Resume /> */}
-      
-    </Bubbles>
+    {
+      page == 'about' ?
+        <Bubbles>
+          <About />
+          <Divider />
+          {/* <Title title="Portfolio" /> */}
+          <Portfolio />
+          {/* <Title title="Contact" /> */}
+          <Contact />
+        </Bubbles> :
+        <Resume />
+    }
     <Socials />
-
-    
   </>
 }
 
