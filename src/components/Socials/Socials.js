a3f4c00b22f00b1dda8442e1cc1e52c713dac087
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-11-16 13:31:17 -0600

    fixed errors

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./socials.css";
 import icon1 from "./images/linkedIn3.png";
-import icon2 from "./images/linkedIn2.png";
 import icon3 from "./images/github.png";
 import icon4 from "./images/resume.png";
 
@@ -9,14 +8,14 @@ const Socials = (props) => {
 	return (
 		<div className="socialContainer" style={{height: '10vh', justifyContent: 'center', display: 'flex'}}>
 			<a href="https://www.linkedin.com/in/taylor-aldridge512/" target="blank">
-				<img src={icon1} />
+				<img src={icon1} alt=''/>
 			</a>
-			<a onClick={() => props.setPage("resume")}>
-				<img id="resumeIcon" src={icon4} />
+			<button onClick={() => props.setPage("resume")}>
+				<img id="resumeIcon" src={icon4} alt=''/>
 				<figcaption>Resume</figcaption>
-			</a>
+			</button>
 			<a href="https://github.com/Rolyat512" target="blank">
-				<img src={icon3} />
+				<img src={icon3} alt=''/>
 			</a>
 		</div>
 	);
