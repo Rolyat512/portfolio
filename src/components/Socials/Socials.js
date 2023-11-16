39dbf50d66863cfeff51cc3885a3b07e5214d6fb
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-11-16 13:24:58 -0600

    inline css

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -7,7 +7,7 @@ import icon4 from "./images/resume.png";
 
 const Socials = (props) => {
 	return (
-		<div className="socialContainer">
+		<div className="socialContainer" style={{height: '10vh', justifyContent: 'center', display: 'flex'}}>
 			<a href="https://www.linkedin.com/in/taylor-aldridge512/" target="blank">
 				<img src={icon1} />
 			</a>
